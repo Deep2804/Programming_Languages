22fd460b1f9f0126826b4d20f7f81ab721149ac4
Deepak Singh <deepak28042002singh@gmail.com>
2024-06-16 22:35:45 +0530

    Inheritance

--- C++/OOPS/Inheritance.cpp
+++ C++/OOPS/Inheritance.cpp
@@ -7,23 +7,38 @@ public:
     string name;
     int age; 
 
-    // Person(string name, int age){
-    //     this->name = name;
-    //     this->age = age;
-    // }  
-    Person(){
-        
-    } 
+    Person(string name, int age){
+        this->name = name;
+        this->age = age;
+    }  
+    // Person(){
+    //    cout<<"Parent Constructor"<<endl; 
+    // } 
 };
 
 class Student : public Person{
     // name, age, rollno
+public:
+    int rollno;
+
+    Student(string name, int age, int rollno) : Person(string name, int age){
+        this->rollno = rollno;
+    }
+
+
+    void getInfo(){
+        cout<<"name: "<<name<<endl;
+        cout<<"age: "<<age<<endl;
+        cout<<"rollno: "<<rollno<<endl;
+    }    
 
 };
 
 
 int main(){
-
+    Student s1("Deepak Singh",);
+    
+    s1.getInfo();
 
 
     return 0;
