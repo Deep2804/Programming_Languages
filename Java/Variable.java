a9a79245d0cb10909c264c285c05c64885d67fbf
Deepak Singh <deepak28042002singh@gmail.com>
2024-06-18 22:31:57 +0530

    variables

--- Java/Variable.java
+++ Java/Variable.java
@@ -1,7 +1,10 @@
 public class Variable {
     public static void main(String[] args) {
-      int myNum;
-      myNum = 12;
+      // int myNum;
+      // myNum = 12;
+      // System.out.println(myNum);
+      int myNum = 12;
+      myNum = 20;
       System.out.println(myNum);
     }
   }
