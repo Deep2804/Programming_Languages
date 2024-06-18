784d659afc20b5b01047238a1a8bd359e5b23f41
Deepak Singh <deepak28042002singh@gmail.com>
2024-06-18 22:39:47 +0530

    variable

--- Java/Variable.java
+++ Java/Variable.java
@@ -29,7 +29,12 @@ public class Variable {
       // int y = 6;
       // System.out.println(x + y);
 
-      int x = 5, y = 6, z = 50;
+      // int x = 5, y = 6, z = 50;
+      // System.out.println(x + y + z);
+
+
+      int x, y, z;
+      x = y = z = 50;
       System.out.println(x + y + z);
     }
   }
