164b63b360eaa03f45c250bf282e4b7913718342
Deep2804 <deepak28042002singh@gmail.com>
2024-06-27 23:28:18 +0530

    variables in JS

---
+++ Javascript/Lecture 1/Variables.js
@@ -0,0 +1,22 @@
+// FullName  = "Deepak Singh";
+// age = 24;
+// price= 99.99
+// x = null;
+// y = undefined;
+// isFollow = true;
+// console.log(FullName);
+// console.log(age);
+// console.log(price);
+// console.log(x);
+// console.log(y);
+// console.log(isFollow);
+
+fullName = "Deepak Singh";
+_fullName = "Rudhra Singh";
+$fullName = "Pratap Singh";
+// 123fullName = "Pratap Singh";
+
+
+console.log($fullName);
+
+
