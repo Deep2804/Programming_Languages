a321d76cc88cf433d7872a66d5ac992ea3577952
Deep2804 <deepak28042002singh@gmail.com>
2024-06-27 23:27:26 +0530

    Keywords in JS

---
+++ Javascript/Lecture 1/Keywords.js
@@ -0,0 +1,75 @@
+//var - variable can be re-declared and updated. A global scope variable.
+// Let - variable cannot be redeclared but can be updated.A block scope variable. 
+// const - variable cannot be redeclared or updated. A block scope variable. 
+//
+// let fullName = "Deepak Singh";
+// let age = 22;
+// let totalPrice = 25000;
+
+// console.log(fullName);
+// console.log(age);
+// console.log(totalPrice);
+
+
+//var - variable can be re-declared and updated.
+// var age = 24;
+
+// var age = 40;
+
+// var age = 59;
+
+// console.log(age);
+
+// var - A global scope variable
+
+//  var price = 25000;
+{
+    var price = 25000;
+     
+}
+console.log(price);
+
+
+// Let - variable cannot be redeclared but can be updated
+// let age = 24;
+
+// age = 40;
+
+// age = 59;
+
+// console.log(age);
+
+// let - A block scope variable
+
+// {
+//     let age = 22;
+
+//     console.log(age);
+
+// }
+
+
+
+
+// const - variable cannot be redeclared or updated.
+// const age = 24;
+
+// age = 40;
+
+// age = 59;
+
+// console.log(age);
+
+// const PI = 3.14;
+
+// console.log(PI);
+
+// const - A block scope variable
+
+// {
+//     const PI = 3.14;
+
+//     console.log(PI);
+// }
+ 
+
