2a239afbdc75485accc9c647223564e7e47b56b5
Deep2804 <deepak28042002singh@gmail.com>
2024-06-27 23:28:02 +0530

    Practice Questions

---
+++ Javascript/Lecture 1/Practice_Question.js
@@ -0,0 +1,25 @@
+// Question 1 : Create a const object called "Product" to store information shown in given picture
+
+const Product = {
+    productName: "Ball Pen",
+    rating : 4,
+    offer: 5,
+    price: 7,
+};
+
+console.log(Product);
+
+
+// Question 2 : Create a const object called "Profile" to store information shown in given picture
+
+const Profile = {
+    userName : "Deepak Singh",
+    postCount : 200,
+    followerCount : 2510000,
+    followingCount: 200,
+    isFollow : true,
+    isMessage : false,
+
+
+};
+console.log(Profile);
