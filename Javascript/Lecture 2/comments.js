48aec15d2347499e7d9ca8874ab4bbf6bcd78f0b
Deep2804 <deepak28042002singh@gmail.com>
2024-06-28 22:54:17 +0530

    Comments in JS

---
+++ Javascript/Lecture 2/comments.js
@@ -0,0 +1,8 @@
+//This is a single line comment 
+//This code print Hello world! in console
+console.log("Hello World!");
+
+//This is a multiline comment
+/* This code print hello world
+ in the window console
+ */
