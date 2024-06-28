9a4e440f3cd990dafab5eeafc5d3fdc21fc9aa83
Deep2804 <deepak28042002singh@gmail.com>
2024-06-28 22:55:10 +0530

    Conditional Statement

---
+++ Javascript/Lecture 2/Conditional_Statement.js
@@ -0,0 +1,39 @@
+// Conditional Statement - to implement some condition on code
+// 1. if statement
+// 2. if-else statement
+// 
+
+//1. If statement
+// let age = 15;
+// let mode = "dark";
+// let color;
+// if(mode === "dark"){
+//     color = "black";
+// }
+
+// if(mode === "light"){
+//     color = "white";
+// }
+
+// console.log(color);
+
+
+
+// if(age >= 18){
+//     console.log("You can Vote");
+// }
+
+// if(age < 18){
+//     console.log("You cannot Vote");
+// }
+
+// 2. if-else statement
+
+let age = 25;
+
+if(age >= 18){
+    console.log("You can Vote");
+}
+else {
+    console.log("You cannot Vote");
+}
