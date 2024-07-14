7892a5db3f6ab7b4e5af28ac7aaad10b200a702f
Deep2804 <deepak28042002singh@gmail.com>
2024-07-14 09:17:18 +0530

    If-else

---
+++ C++/Basic Questions/Day 1/4. If-else.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    if(n <m){
+        cout<<"lesser";
+    }
+    else if(n==m){
+        cout<<"equal";
+    }
+    else {
+        cout<<"greater";
+    }
+
+    return 0;
+}
